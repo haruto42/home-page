1f94067ab1baef50f1b67c8683195bf7a0c16812
haruto42 <haruto@example.com>
2026-04-26 20:14:45 +0900

    ver1.1.0-beta.2

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -2,9 +2,8 @@ export function onRequest(context) {
   const url = new URL(context.request.url);
 
   // ルート（/）に来たら /home/ にリダイレクト
-  if (url.pathname === "/") {
-    url.pathname = "/home/";
-    return Response.redirect(url.toString(), 302);
+  if (url.pathname === "/" || url.pathname === "") {
+    return Response.redirect(new URL("/home/", url).toString(), 302);
   }
 
   // それ以外はそのまま
